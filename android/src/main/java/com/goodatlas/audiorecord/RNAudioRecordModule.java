cc9681eadadbe528b4e58fe6bd4e89e4df5dfc6a
Gunwoo <gunwoo.dev@gmail.com>
2023-04-04 20:18:01 -0300

    Update RNAudioRecordModule.java

--- android/src/main/java/com/goodatlas/audiorecord/RNAudioRecordModule.java
+++ android/src/main/java/com/goodatlas/audiorecord/RNAudioRecordModule.java
@@ -12,6 +12,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
 
 import java.io.File;
 import java.io.FileInputStream;

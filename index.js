7c75ecd875d48c8f4cff57c6769a1c7c300738cb
Gunwoo <gunwoo.dev@gmail.com>
2023-04-05 19:51:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import { DeviceEventEmitter, NativeModules, NativeEventEmitter, Platform } from 'react-native';
 const { RNAudioRecord } = NativeModules;
 
 const AudioRecord = {};
@@ -9,20 +9,29 @@ AudioRecord.stop = () => RNAudioRecord.stop();
 
 const eventsMap = {
   data: 'data',
-  metering1: "metering1",
-  metering2: "metering2"
+  metering1: 'metering1'
+  // metering2: "metering2"
 };
 
 AudioRecord.on = (event, callback) => {
   const nativeEvent = eventsMap[event];
+
   if (!nativeEvent) {
     throw new Error('Invalid event');
   }
 
-  const EventEmitter = new NativeEventEmitter(RNAudioRecord);
+  if (Platform.OS === 'android') {
+    // // 나중에 이 방식으로도 테스트해보기
+    // const test = DeviceEventEmitter
+    // test.removeAllListeners()
+    const EventEmitter = DeviceEventEmitter.addListener('metering1', callback);
 
-  EventEmitter.removeAllListeners(nativeEvent);
-  return EventEmitter.addListener(nativeEvent, callback);
+    return EventEmitter;
+  } else {
+    const EventEmitter = new NativeEventEmitter(RNAudioRecord);
+    EventEmitter.removeAllListeners(nativeEvent);
+    return EventEmitter.addListener(nativeEvent, callback);
+  }
 };
 
 export default AudioRecord;
